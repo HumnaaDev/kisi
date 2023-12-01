958e059ea8d6da51a12891d0ce71d4543bebab2e
Humna <humna.aleem111@gmail.com>
2023-12-02 00:58:24 +0500

    convert files css to scss and js to tsx

--- kisi/pages/index.tsx
+++ kisi/pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import React from 'react'
+import Head from 'next'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
