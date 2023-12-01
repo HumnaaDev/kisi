9961cbf29ff8a85f0a780efbd5ce31e231ec2e4f
Humna <humna.aleem111@gmail.com>
2023-12-02 01:24:37 +0500

    add typescript and install sass

--- kisi/pages/index.tsx
+++ kisi/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Head from 'next'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
