604540560711c79432083835a26a5e80fb318c25
Humna <humna.aleem111@gmail.com>
2023-12-02 23:43:28 +0500

    add chevron icon on images

--- kisi/components/Image/index.tsx
+++ kisi/components/Image/index.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import styles from "./image.module.scss";
+import ChevronRight from '../../assets/icons/chevron-right.svg'
 
-const Image = ({ title, description, imageUrl}) => {
-  console.log('test', imageUrl)
-  if(imageUrl === undefined) {
-    return <div></div>
-  }
+const Image = ({ title, description, imageUrl }) => {
 
   return (
     <div className={styles["box"]}>
       <div
         className={styles["front"]}
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        style={{ background: `url(${imageUrl}) center/cover` }}
       >
         <div className={styles["overlay"]}>
-          <h2>{title}</h2>
+          <div>
+            <h2>{title}</h2>
+          </div>
+          <div className={styles["icon"]}>
+          <img src={ChevronRight.src} />
+        </div>
         </div>
       </div>
       <div className={styles["back"]}>
         <div className={styles["overlay"]}>
           <h2>{title}</h2>
           <p>{description}</p>
+        </div>
+        <div className={styles["icon"]}>
+          <img src={ChevronRight.src} />
         </div>
       </div>
     </div>
