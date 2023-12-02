dac7023845e13c524bc73b226bc3bd7cc77302be
Humna <humna.aleem111@gmail.com>
2023-12-02 23:44:15 +0500

    get images data from api

--- kisi/components/Mosaic/index.tsx
+++ kisi/components/Mosaic/index.tsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import styles from './mosaic.module.scss';
-import img1 from '../../../images/image.png'
-import img4 from '../../../images/image1.png'
-import img2 from '../../../images/image2.png'
-import img6 from '../../../images/image3.png'
-import img5 from '../../../images/image4.png'
-import img7 from '../../../images/image5.png'
-import img3 from '../../../images/image6.png'
-import { Button } from '../Button';
-import { calculateGridPosition } from '../../util';
-import Image from '../Image'
-
-const emptyImage = {
-    src: ''
-}
-
-const images = [
-    img1, img2, emptyImage, img3, img4, img5, img6, img7 
-]
+import React, { useEffect, useState } from "react";
+import styles from "./mosaic.module.scss";
+import { Button } from "../Button";
+import { calculateGridPosition } from "../../util";
+import Image from "../Image";
 
 export const Mosaic = () => {
+  const [catalogs, setCatalogs] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = async () => {
+    const response = await fetch("/api/images");
+    const data = await response.json();
+    setCatalogs(data);
+  };
+
   return (
-    <div className={styles['main-container']}>
-        <div className={styles['mosaic']}>
-            <div className={styles["heading"]}>Connect People and spaces</div>
-            {
-                images.map((img, index) => 
-                    <div className={`${styles["mosaic-item"]}`} style={calculateGridPosition(index)} >
-                        <Image title={'title'} description={'description'} imageUrl={img.src} />
-                    </div>)
-            }
-            <Button name="Add New Image" type="button"/>
-        </div>
+    <div className={styles["main-container"]}>
+      <div className={styles["mosaic"]}>
+        {catalogs?.length > 0 ?<>
+        <div className={styles["heading"]}>Connect People and spaces</div>
+        {catalogs.map((catalog, index) => (
+          <div
+            key={index}
+            className={`${styles["mosaic-item"]}`}
+            style={calculateGridPosition(index)}
+          >
+            <Image
+              title={catalog.title}
+              description={catalog.description}
+              imageUrl={catalog.image}
+            />
+          </div>
+        ))}
+        <Button onUpload={getImages} name="Add New Image" type="button" />
+        </>: <div className={styles["loading"]}>Loading ...</div>}
+      </div>
     </div>
-   
   );
 };
