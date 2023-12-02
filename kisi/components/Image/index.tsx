9ab466e815b96212b2d108982b972db7e45fd5ae
Humna <humna.aleem111@gmail.com>
2023-12-02 20:09:05 +0500

    create image comp

--- kisi/components/Image/index.tsx
+++ kisi/components/Image/index.tsx
@@ -1,5 +1,25 @@
-export const Image = () => {
-    return (
-        <div>image</div>
-    )
-}
+import React from "react";
+import styles from "./image.module.scss";
+
+const Image = ({ title, description, imageUrl }) => {
+  return (
+    <div className={styles["box"]}>
+      <div
+        className={styles["front"]}
+        style={{ backgroundImage: url(${imageUrl}) }}
+      >
+        <div className={styles["overlay"]}>
+          <h2>{title}</h2>
+        </div>
+      </div>
+      <div className={styles["back"]}>
+        <div className={styles["overlay"]}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Image;
