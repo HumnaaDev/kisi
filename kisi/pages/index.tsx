3d2a4163c474e335311045015ab19c26dfef238a
Humna <humna.aleem111@gmail.com>
2023-12-02 23:43:03 +0500

    implement prettier

--- kisi/pages/index.tsx
+++ kisi/pages/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Mosaic } from '../components/Mosaic'
+import React from "react";
+import { Mosaic } from "../components/Mosaic";
 
 export default function Home() {
   return (
     <div>
       <h1>Your Mosaic App</h1>
       <Mosaic />
-  </div>
-  )
+    </div>
+  );
 }
