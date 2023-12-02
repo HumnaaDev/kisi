e50463e674897e1c5e78d2c97c358a8847be0ecc
Humna <humna.aleem111@gmail.com>
2023-12-02 20:08:45 +0500

    install classnames and display button to add new image

--- kisi/components/Mosaic/index.tsx
+++ kisi/components/Mosaic/index.tsx
@@ -7,40 +7,22 @@ import img6 from '../../../images/image3.png'
 import img5 from '../../../images/image4.png'
 import img7 from '../../../images/image5.png'
 import img3 from '../../../images/image6.png'
+import { Button } from '../Button';
+import { calculateGridPosition } from '../../util';
 
 const images = [
     img1, img2, '', img3, img4, img5, img6, img7
 ]
 
-  const calculateGridPosition = (index) => {
-    const patterns = [
-      [1, 2],
-      [2, 2],
-      [2, 2],
-      [1, 1],
-      [1, 1],
-      [2, 2],
-      [1, 2],
-      [1, 2],
-    ];
-  
-    const patternIndex = index % patterns.length;
-    const [rowSpan, colSpan] = patterns[patternIndex];
-  
-    return {
-      gridRow: index === 0 ? '2 / span 1': `span ${rowSpan}`,
-      gridColumn: `span ${colSpan}`,
-    };
-  };
-
 export const Mosaic = () => {
   return (
     <div className={styles['main-container']}>
         <div className={styles['mosaic']}>
+            <div className={styles["heading"]}>Connect People and spaces</div>
             {
                 images.map((img, index) => <img key={index} className={`${styles["mosaic-item"]} `} style={calculateGridPosition(index)} src={img.src}/>)
             }
-            <button>Add New Image</button>
+            <Button name="Add New Image" type="button"/>
         </div>
     </div>
    
